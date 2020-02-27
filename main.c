5cf10505521cca164ac4d4c4395611d429a5fc5c
Ryan Lamack <c3357711@uon.edu.au>
2020-02-27 18:22:29 +1100

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//#include <stdlib.h>
+
+ int main()
+
+
+{   float x;
+    float y;
+    int k;
+    printf("Enter a number: ");
+    scanf("%f", &x); //note:change%d to %f
+    //complete the y line below
+    y = (x+2)/(x-1);
+    printf("y: %f\n", y);
+    return 0;
+}
+
+ // code implements function:y=2x+3*5
+
